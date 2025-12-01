b3796e4f3c82c13242caa1b4acfc5f15c18ea5ff
Dávid Paksy <paksydavid@gmail.com>
2025-12-01 12:56:00 +0100

    Published site at 989ec0d9787813f45495ead3846170f8a0f677ae.

---
+++ assets/source-repository-Bx_ZJMpT.js
@@ -0,0 +1,52 @@
+import{p as e,w as t}from"./chunk-OIYGIGL5-u-rPwZH2.js";import{M as n}from"./markdown-layout-DRQ1YBds.js";import"./external-link-BRT3Rnas.js";import"./createLucideIcon-2kDJI1LL.js";const o=`<!--
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
+# Overview
+
+Apache HBase™ uses [Git](https://git-scm.com/) to manage its source code. Instructions on Git use can be found at [https://git-scm.com/documentation](https://git-scm.com/documentation).
+
+### Web Browser Access
+
+The following is a link to a browsable version of the source repository:
+
+\`\`\`
+https://gitbox.apache.org/repos/asf?p=hbase.git
+\`\`\`
+
+[Browse the repository](https://gitbox.apache.org/repos/asf?p=hbase.git)
+
+### Anonymous Access
+
+The source can be checked out anonymously from Git with this command (See [https://git-scm.com/docs/git-clone](https://git-scm.com/docs/git-clone)):
+
+\`\`\`bash
+$ git clone git://gitbox.apache.org/repos/asf/hbase.git
+\`\`\`
+
+### Developer Access
+
+Only project developers can access the Git tree via this method (See [https://git-scm.com/docs/git-clone](https://git-scm.com/docs/git-clone)).
+
+\`\`\`bash
+$ git clone https://gitbox.apache.org/repos/asf/hbase.git
+\`\`\`
+
+### Access from Behind a Firewall
+
+Refer to the documentation of the SCM used for more information about access behind a firewall.
+`;function s(){return e.jsx(n,{children:o})}function p({}){return[{title:"Source Repository - Apache HBase"},{name:"description",content:"Access the Apache HBase source code repository."}]}const m=t(function(){return e.jsx(s,{})});export{m as default,p as meta};
