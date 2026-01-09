3d44f923543b1aa3f6f1d69194221204ec86e5c8
jenkins <builds@apache.org>
2026-01-09 14:51:20 +0000

    Published site at f35268aecfbf0fc975f3ab829a512c04f46cb048.

---
+++ assets/mailing-lists-D0r67cce.js
@@ -0,0 +1,31 @@
+import{p as e,w as s}from"./chunk-EPOLDU6W-BScaAzfb.js";import{M as i}from"./markdown-layout-Dg6foF5A.js";import"./link-WMIidchi.js";import"./external-link-DNs9Fh7l.js";import"./createLucideIcon-E1ztnwy0.js";const a=`<!--
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
+# Apache HBase™ Mailing Lists
+
+For each list, links are provided to subscribe, unsubscribe, and view archives.
+
+| Name | Subscribe | Unsubscribe | Post | Archive | Other Archives |
+|------|-----------|-------------|------|---------|----------------|
+| User List | [Subscribe](mailto:user-subscribe@hbase.apache.org) | [Unsubscribe](mailto:user-unsubscribe@hbase.apache.org) | [Post](mailto:user@hbase.apache.org) | [lists.apache.org](https://lists.apache.org/list.html?user@hbase.apache.org) | [dir.gmane.org](https://dir.gmane.org/gmane.comp.java.hadoop.hbase.user) |
+| Developer List | [Subscribe](mailto:dev-subscribe@hbase.apache.org) | [Unsubscribe](mailto:dev-unsubscribe@hbase.apache.org) | [Post](mailto:dev@hbase.apache.org) | [lists.apache.org](https://lists.apache.org/list.html?dev@hbase.apache.org) | [dir.gmane.org](https://dir.gmane.org/gmane.comp.java.hadoop.hbase.devel) |
+| Commits List | [Subscribe](mailto:commits-subscribe@hbase.apache.org) | [Unsubscribe](mailto:commits-unsubscribe@hbase.apache.org) | - | [lists.apache.org](https://lists.apache.org/list.html?commits@hbase.apache.org) | - |
+| Issues List | [Subscribe](mailto:issues-subscribe@hbase.apache.org) | [Unsubscribe](mailto:issues-unsubscribe@hbase.apache.org) | - | [lists.apache.org](https://lists.apache.org/list.html?issues@hbase.apache.org) | - |
+| Builds List | [Subscribe](mailto:builds-subscribe@hbase.apache.org) | [Unsubscribe](mailto:builds-unsubscribe@hbase.apache.org) | - | [lists.apache.org](https://lists.apache.org/list.html?builds@hbase.apache.org) | - |
+| User (ZH) List | [Subscribe](mailto:user-zh-subscribe@hbase.apache.org) | [Unsubscribe](mailto:user-zh-unsubscribe@hbase.apache.org) | [Post](mailto:user-zh@hbase.apache.org) | [lists.apache.org](https://lists.apache.org/list.html?user-zh@hbase.apache.org) | - |
+`;function t(){return e.jsx(i,{children:a})}function u({}){return[{title:"Mailing Lists - Apache HBase"},{name:"description",content:"Subscribe to Apache HBase mailing lists including user, developer, commits, issues, and builds lists."}]}const p=s(function(){return e.jsx(t,{})});export{p as default,u as meta};
